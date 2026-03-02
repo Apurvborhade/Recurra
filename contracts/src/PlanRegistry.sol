1ec1a192c505cedd1d65e2e65071171083fd9fb6
Apurvborhade <apurvaborhadee@gmail.com>
2026-03-02 20:16:34 +0530

    Plan Registry

--- contracts/src/PlanRegistry.sol
+++ contracts/src/PlanRegistry.sol
@@ -11,12 +11,13 @@ struct Plan {
 }
 
 contract PlanRegistry {
-    Plan[] public plans;
-
+    mapping(address => Plan[]) private merchantPlans;
     error ZeroPrice();
     error ZeroInterval();
 
-    constructor(address _merchant, address _token, uint256 _price, uint256 _interval) {
+    constructor() {}
+
+    function createPlan(address _token, uint256 _price, uint256 _interval) external {
         if (_price == 0) {
             revert ZeroPrice();
         }
@@ -24,8 +25,18 @@ contract PlanRegistry {
             revert ZeroInterval();
         }
 
-        Plan memory plan1 = Plan({merchant: _merchant, token: _token, price: _price, interval: _interval, active: true});
-        
-        plans.push(plan1);
+        Plan memory plan = Plan({merchant: msg.sender, token: _token, price: _price, interval: _interval, active: true});
+
+        merchantPlans[msg.sender].push(plan);
+    }
+
+    function getPlan(uint256 planId)
+        external
+        view
+        returns (address merchant, address token, uint256 price, uint256 interval, bool active)
+    {
+        Plan storage plan = merchantPlans[merchant][planId];
+
+        return (plan.merchant, plan.token, plan.price, plan.interval, plan.active);
     }
 }
